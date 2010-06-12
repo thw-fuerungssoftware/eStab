d905438070a1e9fc1db12419ae9e892da85fe6fe
arawerma <arawerma@595569ee-1d76-4581-98bb-cbe32a4b19d9>
2010-06-12 21:57:16 +0000

    Nachzügler...

---
+++ eStab/trunk/language/german/form.php
@@ -0,0 +1,41 @@
+<?php
+
+
+  $langform = array (
+      0   ->  "REPORTING SHEET",
+      1   ->  "EINGANG",
+      2   ->  "Aufnahmevermerk",
+      3   ->  "Datum&Uhrzeit",
+      4   ->  "Zeichen",
+      5   ->  "AUSGANG",
+      6   ->  "Annahme",
+      7   ->  "Uhrzeit",
+      8   ->  "Zeichen",
+      9   ->  "Beförderungsvermerk",
+     10   ->  "Datum&Uhrzeit",
+     11   ->  "Zeichen",
+     12   ->  "Nachweis",
+     13   ->  "E",
+     14   ->  "A",
+     15   ->  "Rufname der Gegenstelle/Spruchkopf",
+     16   ->  "Beförderungsweg:",
+     17   ->  "Durchsage",
+     18   ->  "Spruch",
+     19   ->  "Beförderungshinweis: Tel.",
+     20   ->  "Vorrangstufe",
+     21   ->  "Anschrift",
+     22   ->  "Gesprächsnotiz",
+     23   ->  "Inhalt",
+     24   ->  "Abfassungszeit",
+     25   ->  "Absender",
+     26   ->  "Zeichen",
+     27   ->  "Funktion",
+     28   ->  "Quittung",
+     29   ->  "Uhrzeit",
+     30   ->  "Zeichen",
+     31   ->  "Vermerk",
+
+  ); // array langform
+
+
+?>
