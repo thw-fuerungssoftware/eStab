ffbc52af8d2c386798c15eb3d0185735a743f1f9
toboms <toboms@595569ee-1d76-4581-98bb-cbe32a4b19d9>
2011-02-17 09:00:38 +0000

    admmenue.inc.php: Menü Link zu set_number_after_crash.php gesetzt.

--- eStab/trunk/4fadm/set_number_after_crash.php
+++ eStab/trunk/4fadm/set_number_after_crash.php
@@ -39,18 +39,35 @@ $protokoll = false;
 
 //Datenbank abfrage und in Array ablegen
  $tabelle = $conf_4f_tbl['nachrichten'];
-
-
-
-//########################################## First Page ##########################################
-if($page == 1) {
-
 //DB Einlesen
 $result = mysql_query ("SELECT 04_richtung, 04_nummer FROM  $tabelle ORDER BY 00_lfd DESC");
             while($inhalt = mysql_fetch_array($result, MYSQL_ASSOC)){
             $nummer[] = $inhalt['04_nummer'];
             $richtung[] = $inhalt['04_richtung'];
             }
+
+//Finde letzte Eingangsnummer
+                 for($i=0; $i<=count($richtung); $i++){
+                 if($richtung[$i] == "E")
+                         {
+                         $e_nummer = $i;
+                         break;
+                         }
+                 }
+//Finde letzte Ausgangsnummer
+                 for($i=0; $i<=count($richtung); $i++){
+                 if($richtung[$i] == "A")
+                         {
+                         $a_nummer = $i;
+                         break;
+                         }
+                 }
+
+
+//########################################## First Page ##########################################
+if($page == 1) {
+
+
 //Nachweisung Überprüfen
 if(Nachweisung == "gemeinsam")
          {
@@ -58,7 +75,7 @@ if(Nachweisung == "gemeinsam")
 <script type="text/javascript">
          function checkForm() {
          var strFehler='';
-         if (document.forms[0].ea_nummer.value < <? echo $nummer[0]++; ?>)
+         if (document.forms[0].ea_nummer.value <= <?PHP echo $nummer[0]++; ?>)
          strFehler += "Feld Ein-/Ausgangsnummer fehlerhaft:Mindestwert <? echo $nummer[0]; ?> \n";
 
          if (strFehler.length>0) {
@@ -74,7 +91,7 @@ if(Nachweisung == "gemeinsam")
          echo"Hinweis: Tragen Sie hier die letzte verwendete Nummer ein!";
          echo"<table border=2 cellpadding=5 cellspacing=0 bgcolor=#E0E0E0>";
          echo"<tr><td align=right><b>Ein-/Ausgangsnummer</b><br>Mindestwert: ".$nummer[0]."</td><td>";
-         echo"<input name=ea_nummer type=text size=8>";
+         echo"<input name=ea_nummer_get type=text size=8>";
          echo"</td></tr></table>";
          echo"<input name=page type=hidden value=2 size=8>";
          echo"<input type='submit' value='Absenden'/>";
@@ -82,22 +99,6 @@ if(Nachweisung == "gemeinsam")
          }
 elseif(Nachweisung == "getrennt")
          {
-         //Finde letzte Eingangsnummer
-                 for($i=0; $i<=count($richtung); $i++){
-                 if($richtung[$i] == "E")
-                         {
-                         $e_nummer = $i;
-                         break;
-                         }
-                 }
-         //Finde letzte Ausgangsnummer
-                 for($i=0; $i<=count($richtung); $i++){
-                 if($richtung[$i] == "A")
-                         {
-                         $a_nummer = $i;
-                         break;
-                         }
-                 }
 
 ?>
 <script type="text/javascript">
@@ -123,10 +124,10 @@ elseif(Nachweisung == "getrennt")
          echo"Hinweis: Tragen Sie hier die letzten verwendeten Nummeren ein!";
          echo"<table border=2 cellpadding=5 cellspacing=0 bgcolor=#E0E0E0>";
          echo"<tr><td align=right><b>Eingangsnummer</b><br>Mindestwert: ".$nummer[$e_nummer]."</td><td>";
-         echo"<input name=e_nummer type=text size=8>";
+         echo"<input name=e_nummer_get type=text size=8>";
          echo"</td></tr>";
          echo"<tr><td align=right><b>Ausgangsnummer</b><br>Mindestwert: ".$nummer[$a_nummer]."</td><td>";
-         echo"<input name=a_nummer type=text size=8>";
+         echo"<input name=a_nummer_get type=text size=8>";
          echo"</td></tr></table>";
          echo"<input name=page type=hidden value=2 size=8>";
          echo"<input type='submit' value='Absenden'/>";
@@ -136,25 +137,53 @@ elseif(Nachweisung == "getrennt")
 //########################################## Second Page ##########################################
 if($page == 2){
 //Neue Nummern im System setzen
+
+//Datenbank Eintragungen
+
+
 if(Nachweisung == "gemeinsam")
          {
-         $ea_num = $_GET['ea_nummer'];
-         $eintrag = "INSERT INTO $tabelle (04_richtung, 04_nummer, 12_inhalt)VALUES ('E', '$ea_num', 'eStab System Meldung')";
-         $eintragen = mysql_query($eintrag);
-         if($eintragen) echo "Nachweisnummer (Eingang/Ausgang) wurde gesetzt auf:".$ea_num;
-         $protokoll = true;
+         $ea_num = $_GET['ea_nummer_get'];
+         $inhalt1 = "eStab Systemmeldung.<br><br>Nachrichtenzähler wurde nach Systemausfall auf E/A".$ea_num." erhöht.";
+         if($ea_num > $nummer[0]++)
+                 {
+                 $eintrag = "INSERT INTO $tabelle (04_richtung, 04_nummer, 12_inhalt)VALUES ('E', '$ea_num', $inhalt1)";
+                 $eintragen = mysql_query($eintrag);
+                 if($eintragen) echo "Nachweisnummer (Eingang/Ausgang) wurde gesetzt auf:".$ea_num;
+                 $protokoll = true;
+                 }
+         else
+                 {
+                 echo "<b>Fehler:</b> Ihr eingegebner Wert (".$ea_num.") ist niedriger als der Mindeswert <u>".$nummer[0]++."</u>";
+                 echo"<br><br> <a href=set_number_after_crash.php>Zurück</a> ";
+                 }
          }
 elseif(Nachweisung == "getrennt")
          {
-         $e_num = $_GET['e_nummer'];
-         $a_num = $_GET['a_nummer'];
-         $eintrag = "INSERT INTO $tabelle (03_zeichen, 04_richtung, 04_nummer, 12_inhalt) VALUES ('xxx', 'A', '$a_num', 'eStab System Meldung')";
-         $eintragen = mysql_query($eintrag);
-         if($eintragen) echo "Nachweisnummer (Ausgang) wurde gesetzt auf:".$a_num."<br>";
-         $eintrag = "INSERT INTO $tabelle (04_richtung, 04_nummer, 12_inhalt, 15_quitdatum, 15_quitzeichen) VALUES ('E', '$e_num', 'eStab System Meldung', now(), 'xxx')";
-         $eintragen = mysql_query($eintrag);
-         if($eintragen) echo "Nachweisnummer (Eingang) wurde gesetzt auf:".$e_num;
-         $protokoll = true;
+         $e_num = $_GET['e_nummer_get'];
+         $a_num = $_GET['a_nummer_get'];
+         $enum = $nummer[$e_nummer]+1;
+         $anum = $nummer[$a_nummer]+1;
+         $inhalt = "eStab Systemmeldung.<br><br>Nachrichtenzähler wurde nach Systemausfall auf E".$e_num."/A".$a_num." erhöht.";
+         if($e_num > $enum  && $a_num > $anum )
+                 {
+                 $eintrag = "INSERT INTO $tabelle (04_richtung, 04_nummer, 12_inhalt, 15_quitdatum, 15_quitzeichen) VALUES ('E', '$e_num', '$inhalt', now(), 'xxx')";
+                 $eintragen = mysql_query($eintrag);
+                 if($eintragen) echo "Nachweisnummer (Eingang) wurde gesetzt auf: ".$e_num."<br>";
+
+                 $protokoll = true;
+
+                 $eintrag = "INSERT INTO $tabelle (03_zeichen, 04_richtung, 04_nummer, 12_inhalt) VALUES ('xxx', 'A', '$a_num', '$inhalt')";
+                 $eintragen = mysql_query($eintrag);
+                 if($eintragen) echo "Nachweisnummer (Ausgang) wurde gesetzt auf: ".$a_num;
+                 }
+         else
+                 {
+                 if($e_num <= $enum)   { echo "<b>Fehler:</b> Ihr eingegebner Eingangs-Wert (".$e_num.") ist niedriger als der Mindeswert <u>".$enum."</u><br>"; }
+                 if($a_num <= $anum) { echo "<b>Fehler:</b> Ihr eingegebner Ausgangs-Wert (".$a_num.") ist niedriger als der Mindeswert <u>".$anum."</u>"; }
+
+                 echo"<br><br> <a href=set_number_after_crash.php>Zurück</a> ";
+                 }
          }
 
 
@@ -171,5 +200,6 @@ if($protokoll)
 
 }
 
-echo "<br><br><br><a href=admin.php>Zurück zur Administrations Übersicht</a>";
+echo "<br><br><br><hr><a href=admin.php>Zurück zur Administrations Übersicht</a>";
+
 ?>
