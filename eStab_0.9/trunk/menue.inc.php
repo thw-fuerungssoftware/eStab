07d8f5cfe2fba0afc8781ff98add320dc6a1f95d
arawerma <arawerma@595569ee-1d76-4581-98bb-cbe32a4b19d9>
2011-10-10 20:35:48 +0000

    "Vordrucke" in Menu.inc.php zeigt jetzt auf die vordrucke des aktuellen Einsatzes

--- eStab_0.9/trunk/menue.inc.php
+++ eStab_0.9/trunk/menue.inc.php
@@ -28,13 +28,13 @@ include ("./4fcfg/config.inc.php");
     $menue[1]["pic"]  = $conf_menue ["symbole"]."4fach_aktiv.png";
     $menue[1]["link"] = "./4fach/index.php";
 
-    $menue[3]["text"] = "Nachweisung";
-    $menue[3]["pic"]  = $conf_menue ["symbole"]."nw.png";
-    $menue[3]["link"] = "./4fach/nachwea.php?nwalle";
+	$menue[3]["text"] = "Generierte Vordrucke<BR>(S2)";
+    $menue[3]["pic"]  = "./4fach/design/mr/folder_global.gif";
+    $menue[3]["link"] = "./4fdata/" . $conf_4f_db["datenbank"] . "/vordruck/";
 
-	$menue[5]["text"] = "PDFs & Anhänge";
-    $menue[5]["pic"]  = "./4fach/design/mr/folder_global.gif";
-    $menue[5]["link"] = "./4fdata";
+	$menue[5]["text"] = "Liste aller Meldungen";
+    $menue[5]["pic"]  = $conf_menue ["symbole"]."all_msg.png";
+    $menue[5]["link"] = "./4fueltg/ue_ltg.php";
 
     $menue[7]["text"] = "Infosammunlung BOS";
     $menue[7]["pic"]  = $conf_menue ["symbole"]."merke32.gif";
@@ -45,17 +45,17 @@ include ("./4fcfg/config.inc.php");
     $zusatz_menue[1]["link"] = "./4fadm/admin.php";
 
 // rechts
-    $menue[2]["text"] = "Einsatztagebuch";
+    $menue[2]["text"] = "Einsatztagebuch<BR>[S2]";
     $menue[2]["pic"]  = $conf_menue ["symbole"]."etb_aktiv.png";
     $menue[2]["link"] = "./stabetb/etb.php";
 
-    $menue[4]["text"] = "Technisches Betriebsbuch";
+    $menue[4]["text"] = "Technisches Betriebsbuch<BR>[A/W]";
     $menue[4]["pic"]  = $conf_menue ["symbole"]."tbb_aktiv.png";
     $menue[4]["link"] = "./fmtbb/tbb.php";
 
-	$menue[6]["text"] = "Liste aller Meldungen";
-    $menue[6]["pic"]  = $conf_menue ["symbole"]."all_msg.png";
-    $menue[6]["link"] = "./4fueltg/ue_ltg.php";
+    $menue[6]["text"] = "Nachweisung";
+    $menue[6]["pic"]  = $conf_menue ["symbole"]."nw.png";
+    $menue[6]["link"] = "./4fach/nachwea.php?nwalle";
 
         $zusatz_menue[2]["text"] = "Kurzanleitung zur eStab Installation & Nutzung";
     $zusatz_menue[2]["pic"]  = $conf_menue ["symbole"]."icon_handbuch.gif";
