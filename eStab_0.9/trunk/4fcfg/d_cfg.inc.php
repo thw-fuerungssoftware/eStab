28b9ccd1e67a03e96e17a32c27be4d8043d5d232
Hajoland <Hajoland@595569ee-1d76-4581-98bb-cbe32a4b19d9>
2011-10-10 19:55:12 +0000

    Fehlerkorrektur

--- eStab_0.9/trunk/4fcfg/d_cfg.inc.php
+++ eStab_0.9/trunk/4fcfg/d_cfg.inc.php
@@ -4,7 +4,7 @@
 \******************************************************************************/ 
 /** Verzeichnis zwischen Root und eStab**/
 
-$conf_web ["pre_path"] = "estab/"; 
+$conf_web ["pre_path"] = "eStabSF/eStab_0.9/"; 
 
 
 
